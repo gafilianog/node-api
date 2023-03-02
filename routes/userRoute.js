9c1b266309f8e1e9168808cec369331c4bb37c9a
Gafiliano Gapili <gafiliano@gmail.com>
2023-03-02 19:46:37 +0700

    add home route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,10 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
+router.get('/', (_, res) => {
+    res.send(<h1>Hello gaes</h1>)
+});
+router.get('/getall', userController.getAllUsers);
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 router.delete('/delete/:name', userController.deleteUser);
