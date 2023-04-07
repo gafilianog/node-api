a4d1c216f77877754e72eb81c20e6a054e3fc2d1
Gafiliano Gapili <gafiliano@gmail.com>
2023-04-07 16:18:56 +0700

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const router = require('./routes/user');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
